53b07adb9b53b68501ed6297f58e2ecf0dfa00d5
jzad <github@jzadl.xyz>
2026-07-17 11:47:56 -0700

    Add TARGET_ARCH and CPU vars to BoardConfig.mk

--- device/samsung/j7velte/BoardConfig.mk
+++ device/samsung/j7velte/BoardConfig.mk
@@ -2,6 +2,18 @@
 
 -include device/samsung/universal7870-common/BoardConfigCommon.mk
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+
 TARGET_BOARD_ODM_DEVICE := samsungexynos7870
 
 # Kernel
