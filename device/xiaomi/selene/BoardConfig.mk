ad0284d31ed89cada5bc7be4d5c58fca2c7bde52
jzad <github@jzadl.xyz>
2026-07-17 11:49:31 -0700

    Add TARGET_ARCH and CPU vars to BoardConfig.mk

--- device/xiaomi/selene/BoardConfig.mk
+++ device/xiaomi/selene/BoardConfig.mk
@@ -3,6 +3,18 @@
 
 TARGET_BOARD_PLATFORM := mt6768
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+
 # Kernel
 TARGET_KERNEL_CONFIG := selene_defconfig
 TARGET_KERNEL_VARIANT_CONFIG := kernel_defconfig
