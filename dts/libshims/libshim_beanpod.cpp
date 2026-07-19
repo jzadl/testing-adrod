5bbe02d9e40655b0527b2d79fd2f0f7dc73ccd18
jzad <github@jzadl.xyz>
2026-07-19 09:41:26 -0700

    fix: libshim_beanpod assign raw pointer instead of KeymasterKeyBlob

--- dts/libshims/libshim_beanpod.cpp
+++ dts/libshims/libshim_beanpod.cpp
@@ -1,9 +1,12 @@
+#include <cstdint>
+#include <cstddef>
 #include <keymaster/android_keymaster_messages.h>
 
 extern "C" {
 void _ZN9keymaster19GenerateKeyResponseD1Ev() {}
 void _ZN9keymaster17AttestKeyResponseD1Ev() {}
 void _ZN9keymaster16ImportKeyRequest14SetKeyMaterialEPKvm(keymaster::ImportKeyRequest* thisptr, const uint8_t* key_material, size_t length) {
-	thisptr->key_data = keymaster::KeymasterKeyBlob(key_material, length);
+	thisptr->key_data = key_material;
+	thisptr->key_data_length = length;
 }
 }
