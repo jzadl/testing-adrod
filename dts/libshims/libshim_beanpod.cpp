52f664817eeca154cceb64e2b53d8248ecc026e2
jzad <github@jzadl.xyz>
2026-07-18 19:38:25 -0700

    fix: use real shim sources from selene-devs (generic AOSP compat)

--- dts/libshims/libshim_beanpod.cpp
+++ dts/libshims/libshim_beanpod.cpp
@@ -1,30 +1,9 @@
-// Shim for keymaster beanpod service
-// Provides missing destructor and method symbols
+#include <keymaster/android_keymaster_messages.h>
 
-namespace android {
-namespace hardware {
-namespace keymaster {
-namespace V4_0 {
-
-// Empty destructors for response classes
-class GenerateKeyResponse {
-public:
-    ~GenerateKeyResponse() {}
-};
-
-class AttestKeyResponse {
-public:
-    ~AttestKeyResponse() {}
-};
-
-}  // namespace V4_0
-}  // namespace keymaster
-}  // namespace hardware
-}  // namespace android
-
-// Provide the symbols
 extern "C" {
-    void _ZN6android8hardware8keymaster4V4_018GenerateKeyResponseD1Ev() {}
-    void _ZN6android8hardware8keymaster4V4_017AttestKeyResponseD1Ev() {}
-    void _ZN6android8hardware8keymaster4V4_003Imp5SetKeyMaterialEPKhm() {}
+void _ZN9keymaster19GenerateKeyResponseD1Ev() {}
+void _ZN9keymaster17AttestKeyResponseD1Ev() {}
+void _ZN9keymaster16ImportKeyRequest14SetKeyMaterialEPKvm(keymaster::ImportKeyRequest* thisptr, const uint8_t* key_material, size_t length) {
+	thisptr->key_data = keymaster::KeymasterKeyBlob(key_material, length);
+}
 }
--- dts/libshims/libshim_showlogo.cpp
+++ dts/libshims/libshim_showlogo.cpp
@@ -1,29 +1,35 @@
-// Shim for libshowlogo.so
-// Bridges old SurfaceComposerClient/SurfaceControl constructors
-
-#include <surfaceflinger/SurfaceComposerClient.h>
-#include <gui/SurfaceControl.h>
+#include <DisplayConfig.h>
 #include <ui/Size.h>
+#include <gui/SurfaceComposerClient.h>
 
 using namespace android;
 
-// SurfaceComposerClient::createSurface shim (old with SurfaceControl*)
-extern "C" void _ZN7android21SurfaceComposerClient12createSurfaceERKNS_8String8EjjjNS_13PixelFormatEjjPNS_2spINS_7IBinderEEENS_19LayerSettingsAccessE(
-    const String8&, uint32_t, uint32_t, uint32_t, int32_t, uint32_t, uint32_t, sp<IBinder>*, uint32_t) {}
+extern "C" {
 
-// SurfaceControl::getSurface const shim
-extern "C" void _ZNK7android13SurfaceControl10getSurfaceEv() {}
+void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijRKNS_2spINS_7IBinderEEENS_13LayerMetadataEPj(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, const sp<IBinder>& parentHandle, LayerMetadata metadata, uint32_t* outTransformHint);
 
-// getActiveDisplayConfig shim
-extern "C" void _ZN7android21SurfaceComposerClient21getActiveDisplayConfigERKNS_2spINS_7IBinderEEEERNS_16DisplayConfigE(
-    const sp<IBinder>&, DisplayConfig&) {}
+void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, LayerMetadata metadata, uint32_t* outTransformHint) {
+  _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijRKNS_2spINS_7IBinderEEENS_13LayerMetadataEPj(name, w, h, format, flags, nullptr, metadata, nullptr);
+ }
 
-// Transaction::apply with extra oneWay param
-extern "C" void _ZN7android21SurfaceComposerClient11Transaction5applyEb(bool) {}
+void _ZN7android14SurfaceControl10getSurfaceEv(void);
 
-// ui::Size::INVALID
-namespace android {
-namespace ui {
-const Size Size::INVALID{-1, -1};
-}  // namespace ui
-}  // namespace android
+void _ZNK7android14SurfaceControl10getSurfaceEv(void) {
+  _ZN7android14SurfaceControl10getSurfaceEv();
+ }
+
+status_t _ZN7android21SurfaceComposerClient20getActiveDisplayModeERKNS_2spINS_7IBinderEEEPNS_2ui11DisplayModeE(const sp<IBinder>& display, ui::DisplayMode* mode);
+
+status_t _ZN7android21SurfaceComposerClient22getActiveDisplayConfigERKNS_2spINS_7IBinderEEEPNS_13DisplayConfigE(const sp<IBinder>& display, DisplayConfig* config) {
+  return _ZN7android21SurfaceComposerClient20getActiveDisplayModeERKNS_2spINS_7IBinderEEEPNS_2ui11DisplayModeE(display, (ui::DisplayMode*)config);
+}
+
+status_t _ZN7android21SurfaceComposerClient11Transaction5applyEbb(void* thisptr, bool synchronous, bool oneWay);
+
+status_t _ZN7android21SurfaceComposerClient11Transaction5applyEb(void* thisptr, bool synchronous) {
+    return _ZN7android21SurfaceComposerClient11Transaction5applyEbb(thisptr, synchronous, false /* oneWay */);
+}
+
+ui::Size _ZN7android2ui4Size7INVALIDE;
+
+}
